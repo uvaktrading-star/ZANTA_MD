94de9b58eaf19eff8b7c6b1c17350cfb1f39501c
Akashkavindu <143059042+Akashkavindu@users.noreply.github.com>
2025-11-23 05:38:47 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "tYMUDDrD#_COq5GBw_3Md_O8hB7J5Vyu6m_CNfYUfhX5oB9ciYQo",
+SESSION_ID: process.env.SESSION_ID || "8EMVTSRA#fCpxiQwOVXeCSbRK0xONkwilR8rnkLf3aj40vuqdVnk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Akashkavindu/ZANTA_MD/blob/main/images/ChatGPT%20Image%20Nov%2020,%202025,%2009_47_50%20PM.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 ZANTA-MD Is Alive Now😍*\n\n> ZANTA MD WA BOT",
 BOT_OWNER: '94743404814',  // Replace with the owner's phone number
